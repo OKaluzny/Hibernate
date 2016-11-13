2d63cf4ceb6746e48ca65442ddc59642bab165fd
dedamon2016 <pavel.sharipov@gmail.com>
2016-11-13 17:47:48 +0200

    HQLBookRepository.java added

--- hibernate-task2/src/main/java/org/it/discovery/training/hibernate/bootstrap/HibernateStarter.java
+++ hibernate-task2/src/main/java/org/it/discovery/training/hibernate/bootstrap/HibernateStarter.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.it.discovery.training.hibernate.model.Book;
 import org.it.discovery.training.hibernate.model.Publisher;
+import org.it.discovery.training.hibernate.repository.HQLBookRepository;
+import org.it.discovery.training.hibernate.repository.HibernateBookRepository;
 import org.it.discovery.training.hibernate.repository.HibernatePublisherRepository;
 import org.it.discovery.training.hibernate.util.HibernateUtil;
 
@@ -16,16 +18,26 @@ public class HibernateStarter {
     public static void main(String[] args) {
         try (SessionFactory factory = HibernateUtil.getSessionFactory()) {
             HibernatePublisherRepository repository = new HibernatePublisherRepository();
+            HibernateBookRepository bookRepository = new HibernateBookRepository();
+            HQLBookRepository hqlBookRepository = new HQLBookRepository();
             Publisher publisher = new Publisher();
             publisher.setName("Test");
             Book book1 = new Book();
+            book1.setName("Test name first");
             Book book2 = new Book();
+            book2.setName("Test name second");
             publisher.addBook(book1);
             publisher.addBook(book2);
             repository.save(publisher);
             Publisher publisher1 = repository.findById(1);
             System.out.println(publisher1.getBookCount());
-            repository.delete(1);
+            //repository.delete(1);
+            List<Book> testBookList = bookRepository.findWithName("Test name first");
+            System.out.println(testBookList.size());
+            System.out.println(testBookList);
+            List<Book> testHQLBookList = hqlBookRepository.findAll();
+            System.out.println(testHQLBookList.size());
+            System.out.println(testHQLBookList);
 
         }
 
--- hibernate-task2/src/main/java/org/it/discovery/training/hibernate/repository/HQLBookRepository.java
+++ hibernate-task2/src/main/java/org/it/discovery/training/hibernate/repository/HQLBookRepository.java
@@ -7,32 +7,34 @@ import org.hibernate.criterion.Restrictions;
 import org.it.discovery.training.hibernate.model.Book;
 import org.it.discovery.training.hibernate.util.HibernateUtil;
 
+import javax.persistence.Query;
 import java.util.List;
 
 /**
  * Created by Шарипов on 13.11.2016.
  */
-public class HibernateBookRepository implements BookRepository {
+public class HQLBookRepository implements BookRepository {
     @Override
     public List<Book> findAll() {
-
-        return null;
-    }
-
-    @Override
-    public List<Book> findWithName(String name) {
         List<Book> books = null;
         try {
             Session session = HibernateUtil.getSessionFactory().getCurrentSession();
             session.beginTransaction();
-            Criteria criteria = session.createCriteria(Book.class);
-            criteria.add(Restrictions.eq("name", name));
-            books = criteria.list();
+            Query query = session.createQuery("from Book");
+            books = query.getResultList();
             session.getTransaction().commit();
         } catch (HibernateException e) {
             e.printStackTrace();
         }
         return books;
+
+    }
+
+    @Override
+    public List<Book> findWithName(String name) {
+
+
+        return null;
     }
 
     @Override
